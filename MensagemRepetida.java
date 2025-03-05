4dc4542eb18fb88da8548c48b1cc4e6353d064b9
mcossf <158389773+mcossf@users.noreply.github.com>
2025-03-05 12:32:44 -0300

    Exercício 1/15

---
+++ MensagemRepetida.java
@@ -0,0 +1,7 @@
+public class MensagemRepetida {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("Eu gosto de estudar Algoritmos!");
+        }
+    }
+}
